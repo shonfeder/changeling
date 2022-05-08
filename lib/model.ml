1cb43b6df9c3f8548a7b6f7b41e103824a8a3374
Shon Feder <shon.feder@gmail.com>
2022-05-08 01:07:18 -0400

    Add document model of changelog file

--- lib/model.ml
+++ lib/model.ml
@@ -1,40 +1,43 @@
-(* Document model of a keep a changelog style changelog file *)
+(** Document model of a keep a changelog style changelog file *)
 
 type elem = Omd_representation.element
 type md = Omd.t
 
+(** A kind of change *)
 module Change = struct
-  type t =
-    | Breaking_changes
-    | Bug_fixes
-    | Deprecations
-    | Documentation
-    | Features
+  module T = struct
+    (* TODO: make configurable *)
+    type t =
+      | Breaking_changes
+      | Bug_fixes
+      | Deprecations
+      | Features
+      | Documentation
+    [@@deriving eq, ord]
+  end
 
-  (* TODO: make configurable *)
+  include T
+
   let to_string = function
     | Breaking_changes -> "Breaking changes"
+    | Features         -> "Features"
     | Bug_fixes        -> "Bug fixes"
     | Deprecations     -> "Deprecations"
     | Documentation    -> "Documentation"
-    | Features         -> "Features"
+
+  module Map = Map.Make (T)
 end
 
-type section =
-  { kind : Change.t
-  ; changes : md
-  }
-
 type version =
-  { id : string
-  ; summary : md
-  ; date : string option
-  ; sections : section list
+  { id : string  (** The version number (or "unreleased") *)
+  ; date : string option  (** The date when the version was released *)
+  ; summary : md  (** An optional summary of the version *)
+  ; changes : md Change.Map.t  (** A map of change kinds to change lists *)
   }
 
 type t =
-  { title : elem
-  ; summary : md
-  ; unreleased : version
-  ; releases : version list
+  { title : elem  (** The title of the changelog (defaults to "Changelog")*)
+  ; summary : md  (** Summary of the change log *)
+  ; unreleased : version (** The not-yet released set of changes *)
+  ; releases : version list (** All the versions that have been released *)
   }
