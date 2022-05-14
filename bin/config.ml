dce0d479fcfc197deb287e56963bf1fa5ce2237a
Shon Feder <shon.feder@gmail.com>
2022-05-14 19:51:31 -0400

    Add integration tests for changes configuration

--- bin/config.ml
+++ bin/config.ml
@@ -1,7 +1,9 @@
 open Sexplib.Std
 open Containers
+module Sexp = Sexplib.Sexp
 
 type t = { changes : string list [@default []] } [@@deriving sexp]
+(** All application configurations that can be read from a file *)
 
 (** The default change kinds, following the pattern in keep a changelog *)
 let changes =
@@ -9,17 +11,25 @@ let changes =
 
 let default = { changes }
 
-let load ?(file = Fpath.v ".changeling") () =
+let load ?file () =
   let open Result.Infix in
+  let source, file =
+    match file with
+    | None   -> (`Default, Fpath.v ".changeling")
+    | Some f -> (`Configured, f)
+  in
   let* file_exists = Bos.OS.File.exists file in
-  if not file_exists then
-    Ok default
-  else
-    let fname = Fpath.to_string file in
-    let+ s =
-      try Sexplib.Sexp.load_sexp fname |> Result.return with
-      | Sexplib.Sexp.Parse_error _
-      | Failure _ ->
-          Rresult.R.error_msgf "invalid s-expression in config file %s" fname
-    in
-    t_of_sexp s
+  let fname = Fpath.to_string file in
+  match (source, file_exists) with
+  | `Default, false    -> Ok default
+  | `Configured, false ->
+      Rresult.R.error_msgf "configuration file %a cannot be found" Fpath.pp file
+  | _otherwise, true   ->
+  match Sexplib.Sexp.load_sexp fname |> t_of_sexp with
+  | (exception Failure _)
+  | (exception Sexp.Parse_error _) ->
+      Rresult.R.error_msgf "invalid s-expression in configuration file %s" fname
+  | exception Sexplib0.Sexp.Of_sexp_error _ ->
+      (* TODO Print example of valid config name? *)
+      Rresult.R.error_msgf "invalid configuration in file %s" fname
+  | t -> Ok t
--- test/configuration.t/run.t
+++ test/configuration.t/run.t
@@ -1,5 +1,62 @@
-Loading from an invalid configuration file raises an error
+Errors
+======
 
-  $ changeling format CHANGES.md
-  error: invalid s-expression in config file .changeling
+Trying to load from a non-existent file reports an error
+
+  $ changeling format --config non-existent-config CHANGES.md
+  error: configuration file non-existent-config cannot be found
   [1]
+
+Loading invalid configuration reports an error
+
+  $ changeling format --config invalid-config CHANGES.md
+  error: invalid configuration in file invalid-config
+  [1]
+
+Loading invalid s-expression in file reports an error
+
+  $ changeling format --config invalid-sexp-config CHANGES.md
+  error: invalid s-expression in configuration file invalid-sexp-config
+  [1]
+
+Success
+=======
+
+Running without configuration file results in default config
+
+  $ changeling release CHANGES.md 0.0.1 && cat CHANGES.md
+  # Changelog
+  
+  ## Unreleased
+  ### Added
+  ### Changed
+  ### Deprecated
+  ### Removed
+  ### Fixed
+  ### Security
+  ## 0.0.1
+
+Configuration is read from a .changeling file, if it's present
+
+  $ cp CHANGES.md.template CHANGES.md
+  $ echo "((changes (Foo Bar Baz)))" > .changeling
+  $ changeling release CHANGES.md 0.0.1 && cat CHANGES.md
+  # Changelog
+  
+  ## Unreleased
+  ### Foo
+  ### Bar
+  ### Baz
+  ## 0.0.1
+
+Configuration source can be set via CLI
+
+  $ cp CHANGES.md.template CHANGES.md
+  $ changeling release --config valid-config-file CHANGES.md 0.0.1 && cat CHANGES.md
+  # Changelog
+  
+  ## Unreleased
+  ### Features
+  ### Bug fixes
+  ### Documentation
+  ## 0.0.1
